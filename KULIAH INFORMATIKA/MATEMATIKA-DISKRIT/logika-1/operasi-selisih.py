820ffff2689fc0ad02ea186dbb07da2e39cf9078
Dicky_asqaelani26 <93518546+Dickybulin26@users.noreply.github.com>
2022-12-28 22:24:29 +0700

    Delete KULIAH INFORMATIKA/MATEMATIKA-DISKRIT/logika-1 directory

--- KULIAH INFORMATIKA/MATEMATIKA-DISKRIT/logika-1/operasi-selisih.py
+++
@@ -1,8 +0,0 @@
-data1 = {1,2,3,4}
-data2 = {1,2,7,8}
-# menggunakan tanda min (-)
-print(data1-data2)
-
-# meggunakan fungsi difference
-data3 = data1.difference(data2)
-print(data3)
--- KULIAH INFORMATIKA/MATEMATIKA-DISKRIT/logika-1/soal-2.py
+++
@@ -1,8 +0,0 @@
-a = 10
-b =13
-
-print(f"a&b={a&b}")
-
-print(f"a|b={a|b}")
-
-print(f"a^b={a^b}")
